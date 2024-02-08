527ce29b8b84f7b06b10b262b81100a494c4f311
Midgard <malmeidamdam1@gmail.com>
2024-02-08 12:43:25 +0100

    Avanzando menu inferior

---
+++ Midgard_menu_bottom/app/src/androidTest/java/fragmentos/PageFragment.kt
@@ -0,0 +1,33 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.menu_bottom.R
+
+class PageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layout = inflater.inflate(R.layout.fragment_page, container, false)
+        val icon = arguments?.getInt(ARG_ICON, 0) ?: 0
+        layout.findViewById<View>(R.id.imageView).setBackgroundResource(icon)
+        return layout
+    }
+
+    companion object {
+        private const val ARG_ICON = "ARG_ICON"
+        fun newInstance(@DrawableRes iconId: Int): PageFragment {
+            val fragment = PageFragment()
+            val args = Bundle()
+            args.putInt(ARG_ICON, iconId)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
--- Midgard_menu_bottom/app/src/main/java/com/example/menu_bottom/MainActivity.kt
+++ Midgard_menu_bottom/app/src/main/java/com/example/menu_bottom/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.menu_bottom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
